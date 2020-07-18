afcd386907e46bc3c43b524a766781a70e09ae1c
consuelogoche-1994 <consuelo.goche@gmail.com>
2020-07-18 17:26:45 -0500

    agregando el objeto con los metodos isValid y maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
 
-console.log(validator);
+console.log(validator.isValid("3726266272"));
+console.log(validator.maskify("3726266272"));
+
+
+console.log("contenido de INDEX.JS");
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,79 @@
 const validator = {
-  // ...
+    //Función para 
+  isValid:function isValid(creditCardNumber)
+  {
+    let contenedor = [];
+    //length=método para obtener número de caracteres de la cadena
+    let longIMEI= creditCardNumber.length;
+    let suma=0;
+    //varieble que almacena el valor duplicado
+    let numDupli;
+    let resto;
+   //for para invertir ,duplicar y sumar digitos mayores a dos cicfras
+    for (var i = 0; i<longIMEI; i++) 
+    {
+    //y= número extraido
+    //substr=metodo para obtener un caracter en una posición
+    let y=creditCardNumber.substr(longIMEI-1-i,1);
+    //verifica el resto para ver si es par o impar
+      switch(i%2)
+      {
+    //resto = 0 significa que la posición es par
+        case 0:
+            contenedor[i]=parseInt(y,10);
+        break;
+    //resto = 1 significa que la posición es impar
+        case 1:
+            numDupli=parseInt(y,10)*2;
+            if(numDupli.toString().length==1)
+              {
+                contenedor[i]=numDupli;
+              }
+              else if(numDupli.toString().length==2)
+                {
+                  //convertir a string el número duplicado y asignarlo a w
+                  let w=String(numDupli);
+                  let sumaDig=parseInt(w.substr(0,1))+parseInt(w.substr(1,1));
+                  contenedor[i]=sumaDig;
+                }  
+        break;
+      }
+    let z=contenedor[i];
+    suma=suma+z;
+    resto=suma%10;
+    }
+  //verifica el resto para retornar el valor
+    if(resto==0)
+    {
+    return "válido";
+    }
+      else
+      {
+          return "inválido";
+      }
+  }
+  
+  ,
+  
+  //mostrar solo los últimos 4 digitos
+  maskify:function maskify(creditCardNumber)
+  {
+    let mostrarUltimo=[];
+    let longCard= creditCardNumber.length;
+    //let unirNum;
+    for(var j=0;j<longCard-4;j++)
+      {
+        mostrarUltimo[j]="#";
+      }
+    let unirNum=mostrarUltimo.join("")+creditCardNumber.substr(longCard-4,4);
+
+    return unirNum;
+  
+  }
 };
 
 export default validator;
+
+
+
+
